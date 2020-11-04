d60135fa518838ccdc935db5921bfd1b3748a8bd
kaveendra96 <kaveendra96@gmail.com>
2020-11-04 09:27:28 +0530

    setup redux

--- src/index.js
+++ src/index.js
@@ -1,17 +1,53 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore } from 'redux'
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+const SET_GAME_STARTED='SET_GAME_STARTED'
+const SET_INSTRAUCTIONS_EXPANDED='SET_INSTRAUCTIONS_EXPANDED'
+
+const DEFAULT_SETTINGS={
+  gameStarted:false,
+  instructionsExpanded:false
+}
+
+
+const rootReducer= (state,action) => {
+  console.log('state',state,'action',action)
+  
+  if(action.type === SET_GAME_STARTED){
+    return{
+      gameStarted:action.gameStarted,
+      instructionsExpanded:false
+    }
+  }
+
+  return DEFAULT_SETTINGS}
+
+const store=createStore(rootReducer);
+
+const startGame = () => {
+  return { type:SET_GAME_STARTED,gameStarted:true}
+}
+const cancelGame=() =>{
+  return { type:SET_GAME_STARTED,gameStarted:false}
+}
+const expandInstructions = () => {
+  return {type:SET_INSTRAUCTIONS_EXPANDED,gameStarted:false}
+}
+const cancelInstructions = () => {
+  return {type:SET_INSTRAUCTIONS_EXPANDED,gameStarted:true}
+}
+
+
+store.dispatch(startGame())
+store.dispatch(cancelGame())
+store.dispatch(expandInstructions())
+store.dispatch(cancelInstructions())
+
+
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+    <App />,
+  document.getElementById('root'));
